a1de9e0524072cd6a2b7ae3ab8ea6dbca8c9cb4d
Islam <islamka.1994@mail.ru>
2023-12-16 21:30:04 +0300

    fix tests/test_stack.py file

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,24 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+import pytest
+from src.stack import Node, Stack
+
+# @pytest.fixture
+# def stack():
+#     return Node(5, None)
+
+
+def test_class_node():
+    n1 = Node(5, None)
+    n2 = Node('a', n1)
+    assert n1.data == 5
+    assert n2.data == 'a'
+
+
+def test_class_stack():
+    stack = Stack()
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    assert stack.top.data == 3
+    assert stack.top.next_node.data == 2
+    assert stack.top.next_node.next_node.data == 1
